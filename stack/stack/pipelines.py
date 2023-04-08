2e3c79d6c93cb01e5732a5b390ef4c6013b16189
Geraldo Sobreira Júnior <gsmecanica76@gmail.com>
2023-04-07 21:51:38 -0300

    Update pipelines.py

--- stack/stack/pipelines.py
+++ stack/stack/pipelines.py
@@ -4,11 +4,15 @@ import pymongo
 
 
 class StackPipeline(object):
-
+    
+    # connect the database to the code.
+    
     def __init__(self):
         self.conn = pymongo.MongoClient('localhost', 27017)
         db = self.conn['stackoverflow']
         self.collection = db['questions']
+        
+    # Add to my collection a new item.
 
     def process_item(self, item, spider):
         self.collection.insert_one(dict(item))
